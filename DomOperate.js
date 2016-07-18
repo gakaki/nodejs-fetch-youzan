c1521c617f86861c460cb63d8daf9d6cd065de07
gakaki <gakaki@gmail.com>
2016-07-18 16:00:47 +0800

    xiugai zuobiao

--- DomOperate.js
+++ DomOperate.js
@@ -35,13 +35,13 @@
                 var price         = $(el).find('.goods-price').eq(0).text().trim();
                 var price_int     = price.replace("￥","");
 
-                var td4           = $(el).children('td').eq(3);
+                var td4           = $(el).children('td').eq(4);
                 var uv            = $(td4).children('div').eq(0).text().trim().trimLeft().replace("UV:","");
                 var pv            = $(td4).children('div').eq(1).text().trim().trimLeft().replace("PV:","");
 
-                var stock           = $(el).children('td').eq(4).text().trim();//库存
+                var stock         = $(el).children('td').eq(5).text().trim();//库存
 
-                var sale_num      = $(el).children('td').eq(5).text().trim();
+                var sale_num      = $(el).children('td').eq(6).text().trim();
                 sale_num          = sale_num.replace('-',"0");
 
                 var created_at    = $(el).children('td').eq(7).text().trim();

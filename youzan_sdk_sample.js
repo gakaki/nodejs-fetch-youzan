70df716d55df6b6273716a346fa51a4755a5d7fd
gakaki <gakaki@gmail.com>
2016-08-01 10:11:51 +0800

    add youzan await

--- youzan_sdk_sample.js
+++ youzan_sdk_sample.js
@@ -1,6 +1,17 @@
 
 (async () => {
-	
+
+	function get_brand_name(data){
+		var reg = /【.*】/
+		var res = data.match(reg)
+		console.log(res)
+		res     = res[0]
+		return res
+	}
+
+	console.log(get_brand_name("	MARIPOSA 蝴蝶百合墙饰（一套装）【umbra】"));
+
+
 	// 引入有赞SDK
 	let SDK 		= require('youzan-sdk');
 	// 初始化SDK，在 https://koudaitong.com/v2/apps/open/setting 开启API接口，复制相应 AppID、AppSecert
